82b371ec8074ad5713987fa685d79abfb29e1be0
starmahmoodi <starmahmoodi@gmail.com>
2023-12-30 15:20:33 +0330

    two

--- footer.php
+++ footer.php
@@ -12,19 +12,64 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'khane_zar' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'khane_zar' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'khane_zar' ), 'khane_zar', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="mainFooterBack">
+			<div class="mainFooter mainView">
+				<div class="classfooterColumn1 footerColumns">
+					<h3>تماس با ما</h3>
+					<div class="row">
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.265 2.853a.75.75 0 0 1 .882-.588l1.05.21a2.75 2.75 0 0 1 2.197 2.422l.085.853h13.575a3.05 3.05 0 0 1 2.96 3.79l-1.124 4.49a4.25 4.25 0 0 1-4.123 3.22H7.774a2.75 2.75 0 0 1-2.73-2.422l-.99-8.246-.153-1.535a1.25 1.25 0 0 0-.999-1.102l-1.05-.21a.75.75 0 0 1-.587-.882ZM9 12.75a.75.75 0 0 0 0 1.5h4a.75.75 0 0 0 0-1.5H9Z" fill="#3F3F3F"></path><circle cx="8.5" cy="20" r="1.5" fill="#3F3F3F"></circle><circle cx="17.5" cy="20" r="1.5" fill="#3F3F3F"></circle></svg>
+					<p>09356285529</p>
+					</div>
+					<div class="row">
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.265 2.853a.75.75 0 0 1 .882-.588l1.05.21a2.75 2.75 0 0 1 2.197 2.422l.085.853h13.575a3.05 3.05 0 0 1 2.96 3.79l-1.124 4.49a4.25 4.25 0 0 1-4.123 3.22H7.774a2.75 2.75 0 0 1-2.73-2.422l-.99-8.246-.153-1.535a1.25 1.25 0 0 0-.999-1.102l-1.05-.21a.75.75 0 0 1-.587-.882ZM9 12.75a.75.75 0 0 0 0 1.5h4a.75.75 0 0 0 0-1.5H9Z" fill="#3F3F3F"></path><circle cx="8.5" cy="20" r="1.5" fill="#3F3F3F"></circle><circle cx="17.5" cy="20" r="1.5" fill="#3F3F3F"></circle></svg>
+					<p>info@idechy.ir</p>
+					</div>
+					<div class="row">
+					<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.265 2.853a.75.75 0 0 1 .882-.588l1.05.21a2.75 2.75 0 0 1 2.197 2.422l.085.853h13.575a3.05 3.05 0 0 1 2.96 3.79l-1.124 4.49a4.25 4.25 0 0 1-4.123 3.22H7.774a2.75 2.75 0 0 1-2.73-2.422l-.99-8.246-.153-1.535a1.25 1.25 0 0 0-.999-1.102l-1.05-.21a.75.75 0 0 1-.587-.882ZM9 12.75a.75.75 0 0 0 0 1.5h4a.75.75 0 0 0 0-1.5H9Z" fill="#3F3F3F"></path><circle cx="8.5" cy="20" r="1.5" fill="#3F3F3F"></circle><circle cx="17.5" cy="20" r="1.5" fill="#3F3F3F"></circle></svg>
+					<p>تهران گیشا, بین دوازدهم و چهاردهم,ساختمان اداری خاتم, طیقه دوم, واحد یک</p>
+					</div>
+				</div>
+				<div class="classfooterColumn2 footerColumns">
+					<h3>خدمات</h3>
+					<li>
+						<a href="#">ساختمان سازی</a>
+					</li>
+					<li>
+						<a href="#">نقشه کشی ساختمان</a>
+					</li>
+					<li>
+						<a href="#">واردات وی آر اف</a>
+					</li>
+					<li>
+						<a href="#">نصب وی آر اف</a>
+					</li>
+				</div>
+				<div class="classfooterColumn3 footerColumns">
+					<h3>راهنماها</h3>
+					<li>
+						<a href="#">پروژه ها</a>
+					</li>
+					<li>
+						<a href="#">ارتباط با ما</a>
+					</li>
+					<li>
+						<a href="#">سیستم وی آر اف</a>
+					</li>
+					<li>
+						<a href="#">ثبت درخواست مشاوره</a>
+					</li>
+					<li>
+						<a href="#">مجله خانه زر</a>
+					</li>
+					<li>
+						<a href="#">پروژه ها</a>
+					</li>
+				</div>
+				<div class="classfooterColumn4 footerColumns">
+					<h3>برای ثبت مشاوره همین حالا عضو شوید</h3>
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
