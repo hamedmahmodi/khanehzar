12bbf332b7809705d455a77c86400c573f1e6620
starmahmoodi <starmahmoodi@gmail.com>
2024-01-01 13:32:39 +0330

    2

--- js/swiperSlides.js
+++ js/swiperSlides.js
@@ -6,3 +6,22 @@ var swiper = new Swiper(".projectSwiper", {
       clickable: true,
     },
   });
+
+  var swiper = new Swiper(".serviceSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+  });
