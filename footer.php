3402548d282d4fed2706f1eb6420c400321d36a7
starmahmoodi <starmahmoodi@gmail.com>
2024-01-06 11:42:40 +0330

    44

--- footer.php
+++ footer.php
@@ -68,7 +68,7 @@
 				<div class="classfooterColumn4 footerColumns">
 					<h3>برای ثبت مشاوره همین حالا عضو شوید</h3>
 					<div class= "svg">
-						<svg fill="#cdb578" height="20" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.7 14.7" xml:space="preserve"><path d="M9.98 14.7 2.479 7.35 9.98 0l2.241 2.295L7.062 7.35l5.159 5.055z"/><g/><g/><g/><g/><g/><g/><g/><g/><g/><g/><g/><g/><g/><g/><g/></svg>
+						<svg height="14" width="14" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330" fill="#fff" xml:space="preserve" transform="rotate(90)"><path d="M325.607 79.393c-5.857-5.857-15.355-5.858-21.213.001l-139.39 139.393L25.607 79.393c-5.857-5.857-15.355-5.858-21.213.001-5.858 5.858-5.858 15.355 0 21.213l150.004 150a14.999 14.999 0 0 0 21.212-.001l149.996-150c5.859-5.857 5.859-15.355.001-21.213z"/></svg>
 						<?php echo do_shortcode('[contact-form-7 id="0bdd6e0" title="فرم تماس 1"]') ?>
 					</div>
 
